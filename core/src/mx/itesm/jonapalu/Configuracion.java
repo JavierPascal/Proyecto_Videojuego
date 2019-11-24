157848e33d4df88bfb77a6b2e3f2a899c0033328
luism <a01745997@itesm.mx>
2019-11-23 20:42:58 -0600

    78yh

--- core/src/mx/itesm/jonapalu/Configuracion.java
+++ core/src/mx/itesm/jonapalu/Configuracion.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -30,7 +31,6 @@ class Configuracion implements Screen {
 
     //Fases
     private Stage fasesMenu;
-    private Stage MenuFases;
 
     private Table tabla = new Table();
     private String tiempo;
@@ -104,10 +104,12 @@ class Configuracion implements Screen {
         tabla.setFillParent(true);
         Label nombreLabel = new Label("Nombre:", skin);
         Label tiempoLabel = new Label("Tiempo", skin);
-        tabla.defaults().width(100); // Hace que todas las celdas esten en default.
+        tabla.align(360); // Hace que todas las celdas esten en default.
         tabla.add(nombreLabel);
         tabla.add(tiempoLabel);
         tabla.row();
+
+        tabla.debug();
         //tabla.add(s);
 
 
@@ -131,7 +133,7 @@ class Configuracion implements Screen {
             }
         });
 
-        //Boton sonido
+        /*Boton sonido
         Texture texturasonido = manager.get("Botones/btnSonido.png");
         TextureRegionDrawable sonido = new TextureRegionDrawable
                 (new TextureRegionDrawable(texturasonido));
@@ -164,12 +166,12 @@ class Configuracion implements Screen {
                 super.clicked(event, x, y);
             }
         });
-
+        */
         //Anadir botones
         fasesMenu.addActor(btnRegresar);
         fasesMenu.addActor(tabla);
-        fasesMenu.addActor(btnsonido);
-        fasesMenu.addActor(btnsilencio);
+        //fasesMenu.addActor(btnsonido);
+        //fasesMenu.addActor(btnsilencio);
 
 
         //Cargar las entradas
@@ -194,7 +196,7 @@ class Configuracion implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        tabla.setSize(width, height);
     }
 
     @Override
@@ -216,8 +218,8 @@ class Configuracion implements Screen {
     public void dispose() {
         manager.unload("Botones/btnRegresar.png");
         manager.unload("HUD/fondoGris.png");
-        manager.unload("Botones/btnSonido.png");
-        manager.unload("Botones/btnSilencio.png");
+        //manager.unload("Botones/btnSonido.png");
+        //manager.unload("Botones/btnSilencio.png");
 
 
     }

f1509b80fe2f6db5800a63381f8b4c44a21e6048
Luis Baqueiro <luismibaq@hotmail.com>
2019-11-27 22:24:45 -0600

    Revert "Pantalla configuracion con sonido"
    This reverts commit 29fd6cad

--- core/src/mx/itesm/jonapalu/EstadoJuego.java
+++ core/src/mx/itesm/jonapalu/EstadoJuego.java
@@ -2,15 +2,5 @@ package mx.itesm.jonapalu;
 
 enum EstadoJuego {
     JUGANDO,
-    PAUSA,
-    CRAFTEANDO,
-    TUTORIAL1,
-    TUTORIAL2,
-    TUTORIAL3,
-    TUTORIAL4,
-    TUTORIAL5,
-    TUTORIAL6,
-    TUTORIAL7,
-    TUTORIAL8,
-    TUTORIAL9
+    PAUSA
 }
